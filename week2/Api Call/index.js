ebab9558cca00c7b0cbdc3cf4169c6785af89988
RajibAddya <auddyrajib@gmail.com>
2025-03-08 23:13:20 +0530

    Api Call

---
+++ week2/Api Call/index.js
@@ -0,0 +1,33 @@
+function fakeApiCall(){
+  return new Promise((resolve, reject) => {
+    console.log("Fetching Data.....");
+    setTimeout(() => {
+      if(Math.random() > 0.5){
+        resolve("Api Called Successfully....");
+      }
+      else{
+        reject("Api Call Unsuccessful....");
+      }
+    }, 3000);
+  });
+}
+
+fakeApiCall().
+  then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+async function apicall(){
+  try{
+    const result = await fakeApiCall();
+    console.log(result);
+  }
+  catch(err){
+    console.error(err);
+  }
+}
+
+apicall();
